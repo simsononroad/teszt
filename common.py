d16797fa28d19703e95e62b2926cef07c26ded6f
simsononroad <d-github@kopaszhegy.hu>
2025-11-26 20:24:19 +0100

    szépészeti beavatkozás

--- common.py
+++ common.py
@@ -23,6 +23,32 @@ QUIZZES_DIR = "quizzes"
 IMAGES_DIR = "quiz_images"
 STUDENTS_FILE = "students.json"
 
+
+def gen_task(api_key, task_name, task_topic, num_of_task):
+    from google import genai
+
+    client = genai.Client(api_key=api_key)
+
+
+
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="""Csinálj json formátumban egy feladatsort ilyen formában:
+        [
+            {
+                "question": "Oldd meg az egyenletet: 0.5x + 2 = 3x - 1",
+                "type": "text",
+                "answer": ["1.2"],
+                "points": 1,
+                "match_type": "number"
+            }
+        ]
+        , a témakör legyen: """+ task_topic+f", és a témakörből {num_of_task} mennyiségü kérdést generálj. Csak a json végeredményt mondjad. Használható 'match_type'-ok: contains, number, exact",
+    )
+    with open(f"quizzes/{task_name}.json", "w") as f:
+        f.write(response.text[7:-3])
+
 # Jelszó kezelés
 def hash_password(password):
     """Jelszó hash-elése"""
